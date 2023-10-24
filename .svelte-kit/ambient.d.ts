885c121659061bf5abb0f7b431a644e1aa4e0614
quit <Matt@Matthews-MacBook-Pro.local>
2023-10-24 12:57:09 +0100

    package.json script command pointed to incorrect file

---
+++ .svelte-kit/ambient.d.ts
@@ -0,0 +1,354 @@
+
+// this file is generated — do not edit it
+
+
+/// <reference types="@sveltejs/kit" />
+
+/**
+ * Environment variables [loaded by Vite](https://vitejs.dev/guide/env-and-mode.html#env-files) from `.env` files and `process.env`. Like [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), this module cannot be imported into client-side code. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env).
+ * 
+ * _Unlike_ [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), the values exported from this module are statically injected into your bundle at build time, enabling optimisations like dead code elimination.
+ * 
+ * ```ts
+ * import { API_KEY } from '$env/static/private';
+ * ```
+ * 
+ * Note that all environment variables referenced in your code should be declared (for example in an `.env` file), even if they don't have a value until the app is deployed:
+ * 
+ * ```
+ * MY_FEATURE_FLAG=""
+ * ```
+ * 
+ * You can override `.env` values from the command line like so:
+ * 
+ * ```bash
+ * MY_FEATURE_FLAG="enabled" npm run dev
+ * ```
+ */
+declare module '$env/static/private' {
+	export const NPM_CONFIG_USERCONFIG: string;
+	export const npm_package_dependencies_change_case: string;
+	export const STARSHIP_SHELL: string;
+	export const MANPATH: string;
+	export const npm_package_devDependencies_prettier: string;
+	export const TERM_PROGRAM: string;
+	export const XDG_DATA_HOME: string;
+	export const NODE: string;
+	export const npm_package_dependencies__picocss_pico: string;
+	export const ANDROID_HOME: string;
+	export const GEM_HOME: string;
+	export const INIT_CWD: string;
+	export const npm_package_devDependencies_prettier_plugin_svelte: string;
+	export const npm_package_devDependencies_typescript: string;
+	export const TERM: string;
+	export const SHELL: string;
+	export const npm_package_devDependencies_vite: string;
+	export const npm_package_dependencies__capacitor_cli: string;
+	export const HOMEBREW_REPOSITORY: string;
+	export const TMPDIR: string;
+	export const SQLITE_HISTORY: string;
+	export const BUNDLE_USER_CACHE: string;
+	export const npm_package_scripts_lint: string;
+	export const TERM_PROGRAM_VERSION: string;
+	export const npm_package_devDependencies_eslint_plugin_svelte3: string;
+	export const WINDOWID: string;
+	export const npm_package_scripts_dev: string;
+	export const npm_package_scripts_dev_all: string;
+	export const npm_package_scripts_dev_df: string;
+	export const npm_package_dependencies_lucide_svelte: string;
+	export const npm_package_private: string;
+	export const npm_package_devDependencies__sveltejs_kit: string;
+	export const npm_config_registry: string;
+	export const PNPM_HOME: string;
+	export const USER: string;
+	export const NVM_DIR: string;
+	export const npm_package_scripts_check_watch: string;
+	export const npm_package_dependencies__capacitor_filesystem: string;
+	export const COMMAND_MODE: string;
+	export const RBENV_ROOT: string;
+	export const PNPM_SCRIPT_SRC_DIR: string;
+	export const npm_package_devDependencies__sveltejs_adapter_static: string;
+	export const SSH_AUTH_SOCK: string;
+	export const npm_package_dependencies__felte_reporter_svelte: string;
+	export const __CF_USER_TEXT_ENCODING: string;
+	export const BUNDLE_USER_CONFIG: string;
+	export const npm_package_devDependencies_eslint: string;
+	export const npm_package_devDependencies__typescript_eslint_eslint_plugin: string;
+	export const npm_package_devDependencies_tslib: string;
+	export const npm_execpath: string;
+	export const AWS_CONFIG_FILE: string;
+	export const npm_package_devDependencies_svelte: string;
+	export const npm_package_dependencies_zod_validation_error: string;
+	export const TMUX: string;
+	export const npm_package_dependencies_capacitor_plugin_safe_area: string;
+	export const npm_config_frozen_lockfile: string;
+	export const npm_package_devDependencies__typescript_eslint_parser: string;
+	export const PATH: string;
+	export const CARGO_HOME: string;
+	export const LaunchInstanceID: string;
+	export const __CFBundleIdentifier: string;
+	export const npm_config_init_module: string;
+	export const npm_config_userconfig: string;
+	export const PWD: string;
+	export const npm_command: string;
+	export const JAVA_HOME: string;
+	export const npm_package_scripts_preview: string;
+	export const EDITOR: string;
+	export const KITTY_PID: string;
+	export const npm_lifecycle_event: string;
+	export const LANG: string;
+	export const BUNDLE_USER_PLUGIN: string;
+	export const npm_package_name: string;
+	export const XDG_STATE_HOME: string;
+	export const PYTHONSTARTUP: string;
+	export const NODE_PATH: string;
+	export const npm_package_scripts_build: string;
+	export const TMUX_PANE: string;
+	export const WINEPREFIX: string;
+	export const XPC_FLAGS: string;
+	export const npm_package_dependencies_felte: string;
+	export const PASSWORD_STORE_DIR: string;
+	export const RBENV_SHELL: string;
+	export const npm_package_devDependencies_eslint_config_prettier: string;
+	export const npm_config_node_gyp: string;
+	export const AWS_SHARED_CREDENTIALS_FILE: string;
+	export const XPC_SERVICE_NAME: string;
+	export const npm_package_version: string;
+	export const npm_package_devDependencies__sveltejs_adapter_auto: string;
+	export const npm_package_devDependencies_svelte_check: string;
+	export const SHLVL: string;
+	export const HOME: string;
+	export const npm_package_type: string;
+	export const TERMINFO: string;
+	export const XDG_CONFIG_HOME: string;
+	export const npm_package_dependencies__capacitor_core: string;
+	export const tmux_version: string;
+	export const npm_package_scripts_dev_server: string;
+	export const HOMEBREW_PREFIX: string;
+	export const npm_package_dependencies__capacitor_android: string;
+	export const XDG_CACHE_HOME: string;
+	export const npm_config_store_dir: string;
+	export const NUGET_PACKAGES: string;
+	export const STARSHIP_SESSION_KEY: string;
+	export const LOGNAME: string;
+	export const LESS: string;
+	export const npm_package_scripts_dev_client: string;
+	export const npm_package_scripts_format: string;
+	export const npm_config_cache: string;
+	export const VISUAL: string;
+	export const npm_package_dependencies_zod: string;
+	export const npm_lifecycle_script: string;
+	export const NODE_REPL_HISTORY: string;
+	export const LESSHISTFILE: string;
+	export const TMUX_PLUGIN_MANAGER_PATH: string;
+	export const npm_package_dependencies__capacitor_ios: string;
+	export const npm_package_scripts_dev_nocodb: string;
+	export const npm_package_dependencies__capacitor_geolocation: string;
+	export const KITTY_INSTALLATION_DIR: string;
+	export const KITTY_WINDOW_ID: string;
+	export const npm_config_user_agent: string;
+	export const HOMEBREW_CELLAR: string;
+	export const INFOPATH: string;
+	export const npm_package_dependencies__felte_validator_zod: string;
+	export const npm_package_dependencies__sveltejs_adapter_static: string;
+	export const NB_DIR: string;
+	export const SECURITYSESSIONID: string;
+	export const npm_package_dependencies_svelte_preprocess: string;
+	export const HISTFILE: string;
+	export const npm_package_scripts_check: string;
+	export const npm_config_tmp: string;
+	export const KITTY_PUBLIC_KEY: string;
+	export const COLORTERM: string;
+	export const npm_config_prefix: string;
+	export const npm_node_execpath: string;
+	export const NODE_ENV: string;
+}
+
+/**
+ * Similar to [`$env/static/private`](https://kit.svelte.dev/docs/modules#$env-static-private), except that it only includes environment variables that begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Values are replaced statically at build time.
+ * 
+ * ```ts
+ * import { PUBLIC_BASE_URL } from '$env/static/public';
+ * ```
+ */
+declare module '$env/static/public' {
+	export const PUBLIC_APP_NAME: string;
+	export const PUBLIC_API_KEY: string;
+	export const PUBLIC_HOST_IP: string;
+}
+
+/**
+ * This module provides access to runtime environment variables, as defined by the platform you're running on. For example if you're using [`adapter-node`](https://github.com/sveltejs/kit/tree/master/packages/adapter-node) (or running [`vite preview`](https://kit.svelte.dev/docs/cli)), this is equivalent to `process.env`. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env).
+ * 
+ * This module cannot be imported into client-side code.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/private';
+ * console.log(env.DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ * 
+ * > In `dev`, `$env/dynamic` always includes environment variables from `.env`. In `prod`, this behavior will depend on your adapter.
+ */
+declare module '$env/dynamic/private' {
+	export const env: {
+		NPM_CONFIG_USERCONFIG: string;
+		npm_package_dependencies_change_case: string;
+		STARSHIP_SHELL: string;
+		MANPATH: string;
+		npm_package_devDependencies_prettier: string;
+		TERM_PROGRAM: string;
+		XDG_DATA_HOME: string;
+		NODE: string;
+		npm_package_dependencies__picocss_pico: string;
+		ANDROID_HOME: string;
+		GEM_HOME: string;
+		INIT_CWD: string;
+		npm_package_devDependencies_prettier_plugin_svelte: string;
+		npm_package_devDependencies_typescript: string;
+		TERM: string;
+		SHELL: string;
+		npm_package_devDependencies_vite: string;
+		npm_package_dependencies__capacitor_cli: string;
+		HOMEBREW_REPOSITORY: string;
+		TMPDIR: string;
+		SQLITE_HISTORY: string;
+		BUNDLE_USER_CACHE: string;
+		npm_package_scripts_lint: string;
+		TERM_PROGRAM_VERSION: string;
+		npm_package_devDependencies_eslint_plugin_svelte3: string;
+		WINDOWID: string;
+		npm_package_scripts_dev: string;
+		npm_package_scripts_dev_all: string;
+		npm_package_scripts_dev_df: string;
+		npm_package_dependencies_lucide_svelte: string;
+		npm_package_private: string;
+		npm_package_devDependencies__sveltejs_kit: string;
+		npm_config_registry: string;
+		PNPM_HOME: string;
+		USER: string;
+		NVM_DIR: string;
+		npm_package_scripts_check_watch: string;
+		npm_package_dependencies__capacitor_filesystem: string;
+		COMMAND_MODE: string;
+		RBENV_ROOT: string;
+		PNPM_SCRIPT_SRC_DIR: string;
+		npm_package_devDependencies__sveltejs_adapter_static: string;
+		SSH_AUTH_SOCK: string;
+		npm_package_dependencies__felte_reporter_svelte: string;
+		__CF_USER_TEXT_ENCODING: string;
+		BUNDLE_USER_CONFIG: string;
+		npm_package_devDependencies_eslint: string;
+		npm_package_devDependencies__typescript_eslint_eslint_plugin: string;
+		npm_package_devDependencies_tslib: string;
+		npm_execpath: string;
+		AWS_CONFIG_FILE: string;
+		npm_package_devDependencies_svelte: string;
+		npm_package_dependencies_zod_validation_error: string;
+		TMUX: string;
+		npm_package_dependencies_capacitor_plugin_safe_area: string;
+		npm_config_frozen_lockfile: string;
+		npm_package_devDependencies__typescript_eslint_parser: string;
+		PATH: string;
+		CARGO_HOME: string;
+		LaunchInstanceID: string;
+		__CFBundleIdentifier: string;
+		npm_config_init_module: string;
+		npm_config_userconfig: string;
+		PWD: string;
+		npm_command: string;
+		JAVA_HOME: string;
+		npm_package_scripts_preview: string;
+		EDITOR: string;
+		KITTY_PID: string;
+		npm_lifecycle_event: string;
+		LANG: string;
+		BUNDLE_USER_PLUGIN: string;
+		npm_package_name: string;
+		XDG_STATE_HOME: string;
+		PYTHONSTARTUP: string;
+		NODE_PATH: string;
+		npm_package_scripts_build: string;
+		TMUX_PANE: string;
+		WINEPREFIX: string;
+		XPC_FLAGS: string;
+		npm_package_dependencies_felte: string;
+		PASSWORD_STORE_DIR: string;
+		RBENV_SHELL: string;
+		npm_package_devDependencies_eslint_config_prettier: string;
+		npm_config_node_gyp: string;
+		AWS_SHARED_CREDENTIALS_FILE: string;
+		XPC_SERVICE_NAME: string;
+		npm_package_version: string;
+		npm_package_devDependencies__sveltejs_adapter_auto: string;
+		npm_package_devDependencies_svelte_check: string;
+		SHLVL: string;
+		HOME: string;
+		npm_package_type: string;
+		TERMINFO: string;
+		XDG_CONFIG_HOME: string;
+		npm_package_dependencies__capacitor_core: string;
+		tmux_version: string;
+		npm_package_scripts_dev_server: string;
+		HOMEBREW_PREFIX: string;
+		npm_package_dependencies__capacitor_android: string;
+		XDG_CACHE_HOME: string;
+		npm_config_store_dir: string;
+		NUGET_PACKAGES: string;
+		STARSHIP_SESSION_KEY: string;
+		LOGNAME: string;
+		LESS: string;
+		npm_package_scripts_dev_client: string;
+		npm_package_scripts_format: string;
+		npm_config_cache: string;
+		VISUAL: string;
+		npm_package_dependencies_zod: string;
+		npm_lifecycle_script: string;
+		NODE_REPL_HISTORY: string;
+		LESSHISTFILE: string;
+		TMUX_PLUGIN_MANAGER_PATH: string;
+		npm_package_dependencies__capacitor_ios: string;
+		npm_package_scripts_dev_nocodb: string;
+		npm_package_dependencies__capacitor_geolocation: string;
+		KITTY_INSTALLATION_DIR: string;
+		KITTY_WINDOW_ID: string;
+		npm_config_user_agent: string;
+		HOMEBREW_CELLAR: string;
+		INFOPATH: string;
+		npm_package_dependencies__felte_validator_zod: string;
+		npm_package_dependencies__sveltejs_adapter_static: string;
+		NB_DIR: string;
+		SECURITYSESSIONID: string;
+		npm_package_dependencies_svelte_preprocess: string;
+		HISTFILE: string;
+		npm_package_scripts_check: string;
+		npm_config_tmp: string;
+		KITTY_PUBLIC_KEY: string;
+		COLORTERM: string;
+		npm_config_prefix: string;
+		npm_node_execpath: string;
+		NODE_ENV: string;
+		[key: `PUBLIC_${string}`]: undefined;
+		[key: string]: string | undefined;
+	}
+}
+
+/**
+ * Similar to [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), but only includes variables that begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Note that public dynamic environment variables must all be sent from the server to the client, causing larger network requests — when possible, use `$env/static/public` instead.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/public';
+ * console.log(env.PUBLIC_DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ */
+declare module '$env/dynamic/public' {
+	export const env: {
+		PUBLIC_APP_NAME: string;
+		PUBLIC_API_KEY: string;
+		PUBLIC_HOST_IP: string;
+		[key: `PUBLIC_${string}`]: string | undefined;
+	}
+}
---
+++ .svelte-kit/generated/client/nodes/24.js
@@ -0,0 +1,3 @@
+import * as universal from "../../../../src/routes/(app)/products/+page.ts";
+export { universal };
+export { default as component } from "../../../../src/routes/(app)/products/+page.svelte";
